e38fe9b940f9f9dcfe0568b3c92fd776feab9fb6
RuxshonaXakimova <ruxshonaxakimova774@gmail.com>
2023-09-28 18:01:18 +0500

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ let feels = document.querySelectorAll('#feel')
 let spanTop = document.querySelectorAll('.text_top')
 let spansBottom = document.querySelectorAll('.text_bottom')
 let imgs = document.querySelectorAll('img')
+let regex_check = document.querySelectorAll('.regex_check')
+let all = document.querySelector('.all')
+let need = document.querySelector('.need')
+let success = document.querySelector('.success')
+let error = document.querySelector('.error')
+
+
 
 imgs.forEach(img => {
     img.classList.add('none')
@@ -17,6 +24,29 @@ feels.forEach(feel => {
     feel.style.borderColor = 'blue'
 })
 
+
+
+let pattern = {
+    name: /^[a-z ,.'-]+$/i,
+    email: /^\S+@\S+\.\S+$/,
+    phone: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+    age: /^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/
+}
+
+regex_check.forEach(regex => {
+    regex.onkeyup = () => {
+        if(pattern[regex.getAttribute('name')].test(regex.value)){
+            regex.style.borderColor = 'blue'
+        } else {
+            regex.style.borderColor = 'red'
+        }
+    }
+})
+
+
+
+
+let success_count = 12
 
 form.onsubmit = (event) => {
     event.preventDefault();
@@ -36,7 +66,7 @@ form.onsubmit = (event) => {
             spanTop[idx].style.color = 'blue'
             spansBottom[idx].innerHTML = 'Need to feel'
             spansBottom[idx].style.color = 'blue'
-            imgs[idx].classList.add('none')
+            imgs[idx].classList.add('none')  
         }
     })
 
@@ -57,8 +87,21 @@ function submitFunction(form) {
         user[key] = value
     })
 
+    inps.forEach(inp => {
+        if(inp.value.length == 0) {
+            success_count--
+        }
+    })
     console.log(user);
 
     form.reset();
+
     
+    success.innerHTML = `Success:${success_count}/12`
+    error.innerHTML = `Error:${12-success_count}/12`
 }
+
+
+all.innerHTML = `All:${inps.length}`
+need.innerHTML = `Need:${feels.length}`
+
